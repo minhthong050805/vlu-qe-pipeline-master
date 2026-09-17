86b7d76d532179bd181f8b6245cc5634db764085
minhthong050805 <minhthong050805@gmail.com>
2026-09-17 17:07:05 +0700

    Change admin password from '9999' to '123'

--- auth.js
+++ auth.js
@@ -3,7 +3,7 @@ function login(username, password) {
   if (!username || !password) {
     return false;
   }
-  if (username === 'admin' && password === '9999') {
+  if (username === 'admin' && password === '123') {
     return true;
   }
   return false;
